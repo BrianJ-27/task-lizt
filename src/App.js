ac9fdd9a0ad6f19e664a89d79a2fb890f6b4743c
Brian Johnson <johnson8727@gmail.com>
2021-09-16 13:07:31 -0400

    adding new component files to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
+import "./App.css";
+import React, { useState } from 'react';
 
-import './App.css';
+const App = () => {
 
-function App() {
+  const [formData, setFormData] = useState({
+    newTask: '',
+    filterTask: ''
+  });
+
+  const handleUpdate = (field, { target }) => {
+      setFormData({
+        ...formData,
+        [field]: target.value,
+      });
+  };
+
   return (
-    <div className="App">
-     <h1>This is my App</h1>
-    </div>
+    <main className="App">
+      <div>
+        <h1>Daily Task List</h1>
+        <span id="date-today"></span>
+      </div>
+      <div>
+        <form>
+          <fieldset>
+            <div>
+            <legend>Opening Tasks</legend>
+            <label htmlFor="task"></label>
+            <input 
+              id="task" 
+              type="text" 
+              name="task" 
+              placeholder="New Task" 
+              value={formData.newTask}
+              onChange={e => handleUpdate('newTask', e)}
+              required
+              />
+            </div>
+            <button type="submit" value="Add Task" className="add_tasks btn">
+              Add Task
+            </button>
+          </fieldset>
+          <fieldset>
+            <legend>My Tasks</legend>
+            <div className="input-field col">
+            <label htmlFor="filter"></label>
+              <input
+                id="filter"
+                type="text"
+                name="filter"
+                placeholder="Filter Tasks"
+                value={formData.filterTask}
+                onChange={e => handleUpdate('filterTask', e)}
+              />
+            </div>
+            <div className="col">
+              <ul className="collection"></ul>
+              <button type="submit" value="Clear Task" className="clear_tasks btn">
+                Clear Tasks
+              </button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    </main>
   );
 }
 
